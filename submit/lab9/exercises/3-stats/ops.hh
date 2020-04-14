5f93a2ecdb97deb211a6c3686f1981f3997aaf74
Tony Man <tman1@cs.binghamton.edu>
2020-04-13 20:44:38 -0400

    submitted lab 9

---
+++ submit/lab9/exercises/3-stats/ops.hh
@@ -0,0 +1,24 @@
+#ifndef OPS_HH_
+#define OPS_HH_
+
+/** sorting operations */
+class Ops {
+public:
+
+  /** return < 0, == 0, > 0 if a < b, a == b, a > b */
+  virtual int compare(int a, int b);
+  
+  /** swap a[i] and a[j] */
+  virtual void swap(int a[], int i, int j);
+};
+
+class OpsData: public Ops
+{
+public:
+	int numOfComp, numOfSwap;
+	OpsData() : numOfComp(0), numOfSwap(0) {}
+	int compare(int a, int b);
+	void swap(int a[], int i, int j);
+};
+
+#endif //ifndef OPS_HH_
